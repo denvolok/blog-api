97495255b1ca44898a85e05570a83e15fca27b83
Denis Volok <denis.omen.x@gmail.com>
2020-03-05 06:19:11 +0300

    build: init project setup

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  // parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  settings: {
+    'import/parsers': {
+      "@typescript-eslint/parser": [".ts", ".tsx", '.d.ts']
+    },
+    'import/extensions': [ '.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      }
+    }
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
+  ],
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'lines-between-class-members': ['error', 'always',  {exceptAfterSingleLine: true}],
+    'consistent-return': 0,
+    'import/no-unused-modules': [2, {'unusedExports': true}],
+    'object-curly-newline': ['error', {
+      ImportDeclaration: 'never',
+    }],
+    'no-underscore-dangle': 0,
+    'max-len': 0,
+    'implicit-arrow-linebreak': 0,
+    'no-plusplus': 0,
+    // Typescript
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+  }
+};
