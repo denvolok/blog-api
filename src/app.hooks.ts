4051940bfc0031c167cd40dbcf78eaceeaf9e55d
Denis Volok <denis.omen.x@gmail.com>
2020-03-08 04:44:26 +0300

    feat(validation): protect 'createdAt' and 'updatedAt' fields

--- src/app.hooks.ts
+++ src/app.hooks.ts
@@ -1,14 +1,25 @@
 // Application hooks that run for every service
 // Don't remove this comment. It's needed to format import lines nicely.
 
+import { SyncValidatorFn, validate } from 'feathers-hooks-common';
+import { BadRequest } from '@feathersjs/errors';
+
+const protectTimestamps: SyncValidatorFn = (values) => {
+  if (values.createdAt || values.updatedAt) {
+    throw new BadRequest('Timestamps (\'createdAt\', \'updatedAt\') can\'t be modified');
+  }
+
+  return null;
+};
+
 export default {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [validate(protectTimestamps)],
+    update: [validate(protectTimestamps)],
+    patch: [validate(protectTimestamps)],
     remove: [],
   },
 
